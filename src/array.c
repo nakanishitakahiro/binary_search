151500a01c05a6b2d1cc03e9001693113bb695a3
nakanishitakahiro <taka5115hiro2444@au.com>
2019-06-18 16:25:10 +0900

    message

--- src/array.c
+++ src/array.c
@@ -5,11 +5,20 @@ int k;
 int A[100000];
 
 
+
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
+      int lb = -1 ;
+      int ub = n ;
+      while(ub-lb>1){
+          int m = (lb + ub )/2 ;
+          if(A[m] >= x) ub =  m ;
+          else ib = m ;
+      }
+      return ub ;}
   }
 
 
