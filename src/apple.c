d0dd36f59d291eddad212abfecda27c4fcf53647
nakanishitakahiro <taka5115hiro2444@au.com>
2019-06-19 13:51:17 +0900

    swkoi

--- src/apple.c
+++ src/apple.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 int n;
@@ -11,7 +12,21 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+    lb = 0 ;
+    ub = 1e9+1  ;
+    while(ub-lb>1){
+        int m = (lb + ub )/2 ;
+        for(i = 0; i <n; i++){
+            h = 0
+            h = h+(A[i]+m-1)/m
+        }
+        if(h <= k) ub =  m ;
+        else lb = m ;
+    }
+    printf("%d\n",ub);
+    
+    
+  
 
   return 0;
 }
