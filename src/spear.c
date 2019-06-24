0576512e7e2d8825f2ee003cfbd8d32b418fc059
nakanishitakahiro <taka5115hiro2444@au.com>
2019-06-24 19:57:02 +0900

    19

--- src/spear.c
+++ src/spear.c
@@ -5,14 +5,25 @@ int k;
 int A[100000];
 
 
+int p(int m){
+    int cnt = 0;
+    for(int i = 0; i < n; i++) {cnt += A[i] / m;}
+        return cnt >= k;
+        }
+    
+    
 int main(){
-  int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for(int i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-lb = 
-
-
-  return 0;
+   
+    int lb = 0,ub = 1e9;
+    while(ub - lb > 1){
+        int m = (lb + ub)/2;
+        if(p(m)) lb =  m;
+        else ub = m;
+    }
+    printf("%d\n",lb);
+    return 0;
 }
